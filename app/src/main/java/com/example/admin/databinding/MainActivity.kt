e9605aa06cbb601dce66a3b66f68ce0ffefc83aa
Tehreem <tehreemfazili5@gmail.com>
2019-12-29 18:13:25 +0530

    added changes

--- app/src/main/java/com/example/admin/databinding/MainActivity.kt
+++ app/src/main/java/com/example/admin/databinding/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.admin.databinding
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    override fun onClick(v: View?) {
+       Toast.makeText(this,"View clicked", Toast.LENGTH_LONG).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getList.layoutManager = LinearLayoutManager(this)
-        getList.adapter = RecyclerviewAdapter(userList())
+        getList.adapter = RecyclerviewAdapter(userList(), this)
     }
 
     private fun userList() : List<User>{
--- app/src/main/java/com/example/admin/databinding/RecyclerviewAdapter.kt
+++ app/src/main/java/com/example/admin/databinding/RecyclerviewAdapter.kt
@@ -5,21 +5,22 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.admin.databinding.databinding.ItemListBinding
 
-class RecyclerviewAdapter( val user : List<User> ) : RecyclerView.Adapter<RecyclerviewAdapter.ViewHolder>()
+class RecyclerviewAdapter( val user : List<User>, private val listener :View.OnClickListener) :
+    RecyclerView.Adapter<RecyclerviewAdapter.ViewHolder>()
 {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
 
-       val binding : ItemListBinding = DataBindingUtil.inflate(LayoutInflater.from(parent?.context), R.layout.item_list,parent,false)
+       val binding : ItemListBinding = DataBindingUtil.inflate(LayoutInflater.from(parent?.context), R.layout.item_list,
+           parent,false)
        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
 
-        holder?.bindTo(user[position])
+        holder?.bindTo(user[position],listener)
     }
 
     override fun getItemCount(): Int {
@@ -29,9 +30,9 @@ class RecyclerviewAdapter( val user : List<User> ) : RecyclerView.Adapter<Recycl
 
     class ViewHolder(val binding : ItemListBinding) : RecyclerView.ViewHolder(binding.root){
 
-        fun bindTo(userList : User){
+        fun bindTo(userList : User, listener: View.OnClickListener){
             binding.userModel = userList
-
+            binding.listerner = listener
         }
     }
 }
